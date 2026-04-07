6c75ef16b8d94d25f1ae2355ae9af3efeda740b2
Florian <florian.souquere@gmail.com>
2026-04-07 14:38:15 +0200

    Ajout du composant image avec property binding

---
+++ src/app/image/image.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image',
+  imports: [],
+  templateUrl: './image.html',
+  styleUrl: './image.css',
+})
+export class Image {
+  imageUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
+}
