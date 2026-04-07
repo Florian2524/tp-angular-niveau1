a5bb127a909138aa38f419c15dabdfbaade81df5
Florian <florian.souquere@gmail.com>
2026-04-07 14:49:54 +0200

    Ajout du composant counter avec event binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Welcome } from './welcome/welcome';
 import { Image } from './image/image';
+import { Counter } from './counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Welcome, Image],
+  imports: [RouterOutlet, Welcome, Image, Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/counter/counter.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter {
+  count = 0;
+
+  increment() {
+    this.count++;
+  }
+}
