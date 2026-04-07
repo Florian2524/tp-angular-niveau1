082384e59773e2930122d095b6abc3171ac0f2c5
Florian <florian.souquere@gmail.com>
2026-04-07 11:26:18 +0200

    Ajout du composant welcome avec interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Welcome } from './welcome/welcome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Welcome],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
