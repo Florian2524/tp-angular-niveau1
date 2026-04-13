39f8fcdee70bbfaedb29739cc7affa7249acbd8d
Florian <florian.souquere@gmail.com>
2026-04-13 11:55:49 +0200

    Mise en place du routing avec redirection et page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { Welcome } from './welcome/welcome';
+import { Image } from './image/image';
+import { Counter } from './counter/counter';
+import { Input } from './input/input';
+import { List } from './list/list';
+import { Toggle } from './toggle/toggle';
+import { Data } from './data/data';
+import { NotFound } from './not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'exercice1', pathMatch: 'full' },
+  { path: 'exercice1', component: Welcome },
+  { path: 'exercice2', component: Image },
+  { path: 'exercice3', component: Counter },
+  { path: 'exercice4', component: Input },
+  { path: 'exercice5', component: List },
+  { path: 'exercice6', component: Toggle },
+  { path: 'exercice7', component: Data },
+  { path: '**', component: NotFound }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Welcome } from './welcome/welcome';
-import { Image } from './image/image';
-import { Counter } from './counter/counter';
-import { Input } from './input/input';
-import { List } from './list/list';
-import { Toggle } from './toggle/toggle';
-import { Data } from './data/data';
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Welcome, Image, Counter, Input, List, Toggle, Data],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('tp-angular-niveau1');
 }
